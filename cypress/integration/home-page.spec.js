873483e663d6cfeb3cdb3e30ea334efb13e76d3f
WENTAO WU <872257263@qq.com>
2020-11-01 19:00:01 +0000

    E2E tests - Home page - Filtering by title

--- cypress/integration/home-page.spec.js
+++ cypress/integration/home-page.spec.js
@@ -1,5 +1,26 @@
+let movies;    // List of movies from TMDB
+
+const filterByTitle = (movieList, string) =>
+  movieList.filter((m) => m.title.toLowerCase().search(string) !== -1);
+
+const filterByGenre = (movieList, genreId) =>
+  movieList.filter((m) => m.genre_ids.includes(genreId));
+
 describe("Home Page", () => {
-    beforeEach(() => {
+  before(() => {
+    // Get movies from TMDB and store in movies variable.
+    cy.request(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${Cypress.env(
+        "TMDB_KEY"
+      )}&language=en-US&include_adult=false&include_video=false&page=1`
+    )
+      .its("body")    // Take the body of HTTP response from TMDB
+      .then((response) => {
+        movies = response.results
+      })
+  })
+
+  beforeEach(() => {
       cy.visit("/");
     });
   
@@ -9,5 +30,34 @@ describe("Home Page", () => {
         cy.get("h2").contains("Discover Movies");
         cy.get(".badge").contains(20);
       });
+    })
+    describe("Filtering", () => {
+      describe("By movie title" ,() => {
+        it("should display movies with 'p ' in the title", () => {
+          const searchString = 'p'
+          const matchingMovies = filterByTitle(movies, searchString );
+          cy.get("input").clear().type(searchString) ;
+          cy.get(".card").should("have.length", matchingMovies.length);
+          cy.get(".card").each(($card, index) => {
+            cy.wrap($card)
+            .find(".card-title")
+            .should("have.text", matchingMovies[index].title);
+          });
+        })
+        it("should display movies with 'o' in the title", () => {
+          const searchString = "o";
+          const matchingMovies = filterByTitle(movies, searchString);
+          cy.get("input").clear().type(searchString);
+          cy.get(".card").should("have.length", matchingMovies.length);
+          cy.get(".card").each(($card, index) => {
+            cy.wrap($card)
+            .find(".card-title")
+            .should("have.text", matchingMovies[index].title);
+          })
+        })
+      })
+      describe("By movie genre" ,() => {
+        // More later
+      })
     })
   })
