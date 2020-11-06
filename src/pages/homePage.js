1de1de86f469ccfceb2e4f2e09976df5926e0c29
WENTAO WU <872257263@qq.com>
2020-11-06 15:22:49 +0000

    Refactor add favorites feature

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -5,12 +5,29 @@ import AddToFavoritesButton from '../components/buttons/addToFavorites'
 
 const MovieListPage = () => {
   const context = useContext(MoviesContext);
-
+  const movies = context.movies.filter((m) => {  // New
+    return !("favorite" in m);
+  });
   return (
       <PageTemplate 
         title='No. Movies'
         movies={context.movies}
-        action={(movie) => {
+        action={(movie) => {const MovieListPage = () => {
+          const context = useContext(MoviesContext);
+          const movies = context.movies.filter((m) => {  // New
+            return !("favorite" in m);
+          });
+        
+          return (
+            <PageTemplate
+              title="No. Movies"
+              movies={movies}  /* Changed */
+              action={(movie) => {
+                return <AddToFavoritesButton movie={movie} />;
+              }}
+            />
+          );
+        };
           return <AddToFavoritesButton movie={movie} /> 
         }}
       />
