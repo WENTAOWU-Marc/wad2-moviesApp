62367ff935c1e17583326d241cf8368886e7179e
WENTAO WU <872257263@qq.com>
2020-12-02 21:23:40 +0000

    CA - add nowplayingpage test

--- cypress/integration/nowplaying-page.spec.js
+++ cypress/integration/nowplaying-page.spec.js
@@ -3,9 +3,9 @@ let nowplaying;
 describe("nowplaying page",() => {
   before(() =>{
     cy.request(
-        `https://api.themoviedb.org/3/discover/movie?api_key=${Cypress.env(
+        `https://api.themoviedb.org/3/now_playing/movie?api_key=${Cypress.env(
           "TMDB_KEY"
-        )}&language=en-US&include_adult=false&include_video=false&page=1`
+        )}&language=en-US&page=1`
       )
         .its("body")    // Take the body of HTTP response from TMDB
         .then((response) => {
@@ -16,10 +16,25 @@ describe("nowplaying page",() => {
   beforeEach(() => {
     cy.visit("/");
   });
-  describe("page test", () => {
+  describe("nowplayingpage test", () => {
     it("displays page header", () => {
       cy.get("h2").contains("Nowplaying Movies");
       cy.get(".badge").contains(20);
     });
   })
+describe("Filtering", () => {
+  describe("By movie title" ,() => {
+    it("should display movies with in the title", () => {
+      const searchString = ''
+      const matchingMovies = filterByTitle(movies, searchString );
+      cy.get("input").clear().type(searchString) ;
+      cy.get(".card").should("have.length", matchingMovies.length);
+      cy.get(".card").each(($card, index) => {
+        cy.wrap($card)
+        .find(".card-title")
+        .should("have.text", matchingMovies[index].title);
+      });
+    })
+})
+});
 });
